c37c59bde79cd874ca7a77f0a95267e00ce65247
Damjan Tadic <damjan.tadic@htecgroup.com>
2023-01-17 14:12:30 +0100

    final commit v2

---
+++ frontend/src/pages/theme.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../components/layout/Header";
+import SingleTheme from "../components/theme/SingleTheme";
+
+const Theme = () => {
+  return (
+    <>
+      <Header />
+      <SingleTheme />
+    </>
+  );
+};
+
+export default Theme;
